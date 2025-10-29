0fe9b025a572f7c8f65b9610a48a2b1ddb7bc8b9
sanjit018 <sanjitgorai716@gmail.com>
2025-10-29 13:30:00 +0530

    Initialize default constant and getting all required images

---
+++ components/Constant.js
@@ -0,0 +1,7 @@
+const Primary = '#1D92FF';
+const White = '#F6F8FC';
+const Grey = 'rgba(0, 0, 0, 0.29)';
+const Dark = 'rgba(43, 36, 36, 1)';
+const Black = 'rgba(0, 0, 0, 1)';
+const Gradient1 = 'rgba(12, 125, 228, 1)';
+const Gradient2 = 'rgba(18, 179, 201, 1)';
